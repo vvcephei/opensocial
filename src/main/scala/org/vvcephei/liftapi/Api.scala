0d2ffde9f8613e160f751d511232947bb9bc0988
John Roesler <jroesler@gmail.com>
2012-12-24 09:33:05 -0600

    init

---
+++ src/main/scala/org/vvcephei/liftapi/Api.scala
@@ -0,0 +1,11 @@
+package org.vvcephei.liftapi
+
+import net.liftweb.http.rest.RestHelper
+import net.liftweb.http.{GetRequest, Req}
+import net.liftweb.json.JsonAST.JString
+
+object Api extends RestHelper{
+  serve {
+    case Req(List("api","test"), "json", GetRequest) => JString("static test")
+  }
+}
