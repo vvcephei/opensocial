d5f4299f289e1c4695bc12dc776c0568d97308d1
John Roesler <jroesler@gmail.com>
2013-01-20 09:41:31 -0600

    Working on first client using spray

--- build.sbt
+++ build.sbt
@@ -7,11 +7,15 @@ scalaVersion := "2.9.1"
 seq(webSettings: _*)
 
 resolvers ++= Seq("Java.net Maven2 Repository" at "http://download.java.net/maven/2/",
-              "Sonatype scala-tools repo" at "https://oss.sonatype.org/content/groups/scala-tools/")
+              "Sonatype scala-tools repo" at "https://oss.sonatype.org/content/groups/scala-tools/",
+              "spray repo" at "http://repo.spray.io",
+              "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/")
 
 libraryDependencies ++= Seq("net.liftweb" %% "lift-webkit" % "2.4" % "compile",
                         "org.mortbay.jetty" % "jetty" % "6.1.26" % "container",
                         "ch.qos.logback" % "logback-classic" % "0.9.26",
                         "org.scalatest" %% "scalatest" % "1.8" % "test",
-                        "com.google.inject" % "guice" % "3.0")
+                        "com.google.inject" % "guice" % "3.0",
+                        "io.spray" % "spray-client" % "1.0-M7",
+                        "com.typesafe.akka" % "akka-actor" % "2.0.5")
 
--- src/main/scala/org/vvcephei/opensocial/ui/CLI.scala
+++ src/main/scala/org/vvcephei/opensocial/ui/CLI.scala
@@ -1,7 +1,38 @@
 package org.vvcephei.opensocial.ui
 
+import akka.actor.{Props, ActorSystem}
+import spray.io.IOExtension
+import spray.can.client.HttpClient
+import spray.client.HttpConduit
+import spray.http.{HttpResponse, HttpMethods, HttpRequest}
+import akka.actor.Status.{Failure, Success}
+
+
 object CLI {
   def main(args: Array[String]) {
     println("getting john's content")
+
+    val targetHost = "www.iana.org"
+    val targetPort = 80
+    val targetPath = "/domains/example"
+
+    implicit val system = ActorSystem()
+    val ioBridge = IOExtension(system).ioBridge()
+    val httpClient = system.actorOf(Props(new HttpClient(ioBridge)))
+    val conduit = system.actorOf(props = Props(new HttpConduit(httpClient, targetHost, targetPort)))
+
+    val pipeline = HttpConduit.sendReceive(conduit)
+
+    val response = pipeline(HttpRequest(method = HttpMethods.GET, uri = targetPath))
+
+    response.onComplete {
+      case Right(response) =>
+        println("result:")
+        println(response.entity.asString)
+        system.shutdown()
+      case Left(error) =>
+        println(error)
+        system.shutdown()
+    }
   }
 }
